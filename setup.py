d271091c0f1c5cee36538f1a45dae56a23f7a697
“ricardtrinchetEF” <ricard.trinchet@ef.com>
2024-05-02 13:47:06 +0200

    Move file

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="starter",
-    version="0.0.0",
+    name="solution",
+    version="0.1",
     description="Starter code.",
     author="Student",
 )
