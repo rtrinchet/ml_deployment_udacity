080daabf84232f213a9b2f4c373e65367e7482dc
“ricardtrinchetEF” <ricard.trinchet@ef.com>
2024-05-05 13:23:40 +0200

    Add packages to setup

--- setup.py
+++ setup.py
@@ -6,5 +6,18 @@ setup(
     description="Starter code.",
     author="Student",
     packages=find_packages(),
+    install_requires=[
+            "numpy",
+            "pandas",
+            "scikit-learn",
+            "pytest",
+            "requests",
+            "fastapi==0.63.0",
+            "uvicorn",
+            "gunicorn",
+            "flake8",
+            "ruff",
+            "mangum"
+    ]
 
 )
